aec652bfb7048c0a8a68b7c430f7b23a532ae727
Lashawn Butler <lashawnbutler50@gmail.com>
2024-04-30 17:38:03 -0400

    completed lab

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(titleName => {
+    const words = titleName.split(' ');
+    
+    const titleCasedWords = words.map(word =>
+      word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    
+    return titleCasedWords.join(' ');
+  });
+};
